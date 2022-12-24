e08274b40a14612539c54238e637ae9f85a000a0
kay404 <kay20475@hotmail.com>
2022-12-24 11:37:02 +0800

    omni-swap

--- pallets/omni-swap/src/lib.rs
+++ pallets/omni-swap/src/lib.rs
@@ -1,5 +1,8 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
+/// Edit this file to define custom logic or remove it if it is not needed.
+/// Learn more about FRAME and the core library of Substrate FRAME pallets:
+/// <https://docs.substrate.io/reference/frame-pallets/>
 pub use pallet::*;
 
 #[cfg(test)]
@@ -8,128 +11,208 @@ mod mock;
 #[cfg(test)]
 mod tests;
 
+#[cfg(feature = "runtime-benchmarks")]
+mod benchmarking;
 
 #[frame_support::pallet]
-pub mod pallet {    
+pub mod pallet {
 	use frame_support::pallet_prelude::*;
 	use frame_system::pallet_prelude::*;
-	use sp_std::vec::Vec;
+	// use sp_runtime::traits::TrailingZeroInput;
 
-    /// Configure the pallet by specifying the parameters and types on which it depends.
-	#[pallet::config]
-	pub trait Config: frame_system::Config {
-		/// Because this pallet emits events, it depends on the runtime's definition of an event.
-		type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
-	}
-
-    #[pallet::pallet]
+	#[pallet::pallet]
 	#[pallet::generate_store(pub(super) trait Store)]
 	pub struct Pallet<T>(_);
 
-    // The pallet's runtime storage items.
-	// https://docs.substrate.io/v3/runtime/storage
-	#[pallet::storage]
-	#[pallet::getter(fn proofs)]
-	// Learn more about declaring storage items:
-	// https://docs.substrate.io/v3/runtime/storage#declaring-storage-items
-	pub type Proofs<T:Config> = StorageMap<_, Blake2_128Concat, Vec<u8>, (T::AccountId, T::BlockNumber)>;
+	/// Configure the pallet by specifying the parameters and types on which it depends.
+	#[pallet::config]
+	pub trait Config: frame_system::Config {
+		/// Because this pallet emits events, it depends on the runtime's definition of an event.
+		type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
+	}
 
-    // Pallets use events to inform users when important changes are made.
-	// https://docs.substrate.io/v3/runtime/events-and-errors
+	#[pallet::storage]
+	#[pallet::getter(fn trading_pairs)]
+	// Learn more about declaring storage items:
+	// https://docs.substrate.io/main-docs/build/runtime-storage/#declaring-storage-items
+	pub type TradingPairs<T:Config> = StorageMap<_, Blake2_128Concat, [u8; 32], (u128, u128)>;
+
+	#[pallet::storage]
+	#[pallet::getter(fn total_liquidity)]
+	// Learn more about declaring storage items:
+	// https://docs.substrate.io/main-docs/build/runtime-storage/#declaring-storage-items
+	pub type TotalLiquidity<T:Config> = StorageMap<_, Blake2_128Concat, [u8; 32], u128>;
+
+	#[pallet::storage]
+	#[pallet::getter(fn liquidity)]
+	// Learn more about declaring storage items:
+	// https://docs.substrate.io/main-docs/build/runtime-storage/#declaring-storage-items
+	pub type Liquidity<T:Config> = StorageMap<_, Blake2_128Concat, ([u8; 32], T::AccountId), u128>;
+	// Pallets use events to inform users when important changes are made.
+	// https://docs.substrate.io/main-docs/build/events-errors/
 	#[pallet::event]
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
 	pub enum Event<T: Config> {
 		/// Event documentation should end with an array that provides descriptive names for event
 		/// parameters. [something, who]
-		ClaimCreated(T::AccountId, Vec::<u8>),
-		ClaimRevoked(T::AccountId, Vec::<u8>),
-		ClaimTransferred(T::AccountId, T::AccountId, Vec::<u8>),
+		// SomethingStored(u32, T::AccountId),
+		TokenPurchase([u8; 32], T::AccountId, u128, u128)
 	}
 
-    // Errors inform users that something went wrong.
+	// Errors inform users that something went wrong.
 	#[pallet::error]
 	pub enum Error<T> {
 		/// Error names should be descriptive.
-		ClaimAlreadyExist,
+		NoneValue,
 		/// Errors should have helpful documentation associated with them.
-		ClaimNotExist,
-		/// There have be doc.
-		OnlyOwnerCanRevoke,
-		NotAbleToTransferToSelf,
-		OnlyOwnerCanTransfer,
-		ClaimLengthError,
+		StorageOverflow,
+		InvalidValue,
+		TradingPairNotExist,
+		InsufficientBAmount,
+		InsufficientAAmount,
+		ExceedDesiredAmount,
+		GetAddress0Failed,
+		InsufficientLiquidity,
+		InsufficientAmount
 	}
 
-    #[pallet::hooks]
-    impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {}
-
-    // Dispatchable functions allows users to interact with the pallet and invoke state changes.
+	// Dispatchable functions allows users to interact with the pallet and invoke state changes.
 	// These functions materialize as "extrinsics", which are often compared to transactions.
 	// Dispatchable functions must be annotated with a weight and must return a DispatchResult.
 	#[pallet::call]
 	impl<T: Config> Pallet<T> {
-		/// An example dispatchable that takes a singles value as a parameter, writes the value to
-		/// storage and emits an event. This function must be dispatched by a signed extrinsic.
-		#[pallet::weight(0)]
-		pub fn create_claim(origin: OriginFor<T>, claim: Vec<u8>) -> DispatchResult {
+
+		/// Convert A token to B token
+		#[pallet::weight(10_000 + T::DbWeight::get().reads_writes(1,1).ref_time())]
+		pub fn atob_swap(origin: OriginFor<T>, trading_pair: [u8; 32], tokens_sold: u128, min_token: u128) -> DispatchResult {
 			let sender = ensure_signed(origin)?;
-
-			ensure!(claim.len() == 8, Error::<T>::ClaimLengthError);
-
-            // Check if the claim exists.
-            ensure!(!Proofs::<T>::contains_key(&claim), Error::<T>::ClaimAlreadyExist);
-
-			// Update storage.
-			<Proofs<T>>::insert(
-                &claim,
-                (sender.clone(), frame_system::Pallet::<T>::block_number())
-            );
-
-			// Emit an event.
-			Self::deposit_event(Event::ClaimCreated(sender, claim));
-			// Return a successful DispatchResultWithPostInfo
+			ensure!(tokens_sold > 0 && min_token > 0, Error::<T>::InvalidValue);
+			let (input_reserve, out_reserve) = TradingPairs::<T>::get(&trading_pair).ok_or(Error::<T>::TradingPairNotExist)?;
+			let tokens_bought: u128 = get_input_price(tokens_sold, input_reserve, out_reserve);
+			ensure!(tokens_bought >= min_token, Error::<T>::InvalidValue);
+			<TradingPairs<T>>::insert(
+				&trading_pair,
+				(input_reserve + tokens_sold, out_reserve - tokens_bought)
+			);
+			Self::deposit_event(Event::TokenPurchase(trading_pair, sender, tokens_sold, tokens_bought));
 			Ok(())
 		}
 
-		#[pallet::weight(0)]
-		pub fn revoke_claim(origin: OriginFor<T>, claim: Vec<u8>) -> DispatchResult {
+		/// Convert B token to A token
+		#[pallet::weight(10_000 + T::DbWeight::get().reads_writes(1,1).ref_time())] 
+		pub fn btoa_swap(origin: OriginFor<T>, trading_pair: [u8; 32], tokens_sold: u128, min_token: u128) -> DispatchResult{
 			let sender = ensure_signed(origin)?;
-
-            // Check if the claim exists.
-            let (owner, _) = Proofs::<T>::get(&claim).ok_or(Error::<T>::ClaimNotExist)?;
-
-            ensure!(owner == sender, Error::<T>::OnlyOwnerCanRevoke);
-
-            // Update storage
-            Proofs::<T>::remove(&claim);
-
-            Self::deposit_event(Event::ClaimRevoked(owner, claim));
-
+			ensure!(tokens_sold > 0 && min_token > 0, Error::<T>::InvalidValue);
+			let (input_reserve, out_reserve) = TradingPairs::<T>::get(&trading_pair).ok_or(Error::<T>::TradingPairNotExist)?;
+			let tokens_bought = get_input_price(tokens_sold, out_reserve, input_reserve);
+			ensure!(tokens_bought >= min_token, Error::<T>::InvalidValue);
+			<TradingPairs<T>>::insert(
+				&trading_pair,
+				(input_reserve - tokens_sold, out_reserve + tokens_sold)
+			);
+			Self::deposit_event(Event::TokenPurchase(trading_pair, sender, tokens_sold, tokens_bought));
 			Ok(())
 		}
 
-		#[pallet::weight(0)]
-		pub fn transfer_claim(origin: OriginFor<T>, receiver: T::AccountId, claim: Vec<u8>) -> DispatchResult {
+		#[pallet::weight(10_000 + T::DbWeight::get().reads_writes(1,1).ref_time())] 
+		pub fn add_liquidity(origin: OriginFor<T>, trading_pair: [u8; 32], amount_a_desired: u128, amount_b_desired: u128, amount_a_min: u128, amount_b_min: u128) -> DispatchResult {
 			let sender = ensure_signed(origin)?;
+			ensure!(amount_a_desired > 0 && amount_b_desired > 0, Error::<T>::InvalidValue);
+			let tranding_pair = TradingPairs::<T>::get(&trading_pair);
+			let amount_a: u128;
+			let amount_b: u128;
+			if tranding_pair.is_some() {
+				let (reserve_a, reserve_b) = TradingPairs::<T>::get(&trading_pair).ok_or(Error::<T>::TradingPairNotExist)?;
+				let amount_b_optimal = quote(amount_a_desired, reserve_a, reserve_b);
+				if amount_b_optimal <= amount_b_desired {
+					ensure!(amount_b_optimal > 0 && amount_b_min > 0, Error::<T>::InsufficientBAmount);
+					amount_a = amount_a_desired;
+					amount_b = amount_b_optimal;
+				} else {
+					let amount_a_optimal = quote(amount_b_desired, reserve_b, reserve_a);
+					ensure!(amount_a_optimal <= amount_a_desired, Error::<T>::ExceedDesiredAmount);
+					ensure!(amount_a_optimal > 0 && amount_a_min > 0, Error::<T>::InsufficientAAmount);
+					amount_a = amount_a_optimal;
+					amount_b = amount_b_desired;
+				}
+				<TradingPairs<T>>::insert(
+					&trading_pair,
+					(reserve_a + amount_a, reserve_b + amount_b)
+				);
+			} else {
+				amount_a = amount_a_desired;
+				amount_b = amount_b_desired;
+				<TradingPairs<T>>::insert(
+					&trading_pair,
+					(amount_a, amount_b)
+				);
+				<TotalLiquidity<T>>::insert(
+					&trading_pair,
+					0u128
+				);
+			}
 
-			// Can not transfer the claim to self.
-            ensure!(sender != receiver, Error::<T>::NotAbleToTransferToSelf);
+			// TODO call transfer 
+			// transfer A token and B token to MPC address
+			
+			// mint
+			let (balance_a, balance_b) = TradingPairs::<T>::get(&trading_pair).ok_or(Error::<T>::TradingPairNotExist)?;
+			let mut total_supply = TotalLiquidity::<T>::get(&trading_pair).ok_or(Error::<T>::TradingPairNotExist)?;
+			let liquidity: u128;
+			if total_supply == 0 {
+				liquidity = (amount_a * amount_b).pow(2u32).saturating_sub(1000);
+				total_supply = 1000;
+			} else {
+				// liquidity = Math.min(amount0.mul(_totalSupply) / _reserve0, amount1.mul(_totalSupply) / _reserve1);
+				liquidity = (amount_a.saturating_mul(total_supply) / (balance_a - amount_a)).min(amount_b.saturating_mul(total_supply) / (balance_b - amount_b));
+				total_supply += liquidity;
+			}
+			let balances = Liquidity::<T>::get(&(trading_pair, sender.clone())).unwrap_or(0) + liquidity;
+			<Liquidity::<T>>::insert(&(trading_pair, sender), balances);
+			<TotalLiquidity::<T>>::insert(&trading_pair, total_supply);
+			Ok(())
+		}
 
-            // Check if the claim exists.
-            let (owner, _) = Proofs::<T>::get(&claim).ok_or(Error::<T>::ClaimNotExist)?;
+		#[pallet::weight(10_000 + T::DbWeight::get().reads_writes(1,1).ref_time())] 
+		pub fn remove_liquidity(origin: OriginFor<T>, trading_pair: [u8; 32], liquidity: u128, amount_a_min: u128, amount_b_min: u128) -> DispatchResult {
+			let sender = ensure_signed(origin)?;
+			let balances = Liquidity::<T>::get(&(trading_pair, sender.clone())).unwrap_or(0);
+			ensure!(balances >= liquidity, Error::<T>::InvalidValue);
+			
+			// burn
+			let (balance_a, balance_b) = TradingPairs::<T>::get(&trading_pair).ok_or(Error::<T>::TradingPairNotExist)?;
+			<Liquidity::<T>>::insert(&(trading_pair, sender), balances - liquidity);
+			let total_supply = TotalLiquidity::<T>::get(&trading_pair).ok_or(Error::<T>::TradingPairNotExist)?;
+			let amount_a = liquidity.saturating_mul(balance_a) / total_supply;
+			let amount_b = liquidity.saturating_mul(balance_b) / total_supply;
+			ensure!(amount_a >= amount_a_min && amount_b >= amount_b_min, Error::<T>::InsufficientAmount);
 
-            ensure!(owner == sender, Error::<T>::OnlyOwnerCanTransfer);
-
-            // Update storage
-            Proofs::<T>::remove(&claim);
-			<Proofs<T>>::insert(
-                &claim,
-                (receiver.clone(), frame_system::Pallet::<T>::block_number())
-            );
-
-            Self::deposit_event(Event::ClaimTransferred(owner, receiver, claim));
+			<TotalLiquidity::<T>>::insert(&trading_pair, total_supply - liquidity);
+			// MPC transfer A and B token to sender
 
 			Ok(())
 		}
+		
 	}
+
+	// impl<T: Config> Pallet<T> {
+	pub fn get_input_price(input_amount: u128, input_reserve: u128, output_reserve: u128) -> u128 {
+		// ensure!(input_reserve > 0 && output_reserve > 0u128);
+		let numerator: u128 = input_amount * output_reserve;
+		let denominator: u128 = input_reserve + input_amount;
+		numerator / denominator
+	}
+	
+	pub fn get_output_price(output_amout: u128, input_reserve: u128, output_reserve: u128) -> u128 {
+		// ensure!(input_reserve > 0u128 && output_reserve > 0u128);
+		let numerator: u128 = input_reserve * output_amout;
+		let denominator: u128 = output_reserve - output_amout;
+		numerator / denominator
+	}
+
+	/// given some amount of an asset and pair reserves, returns an equivalent amount of the other asset
+	pub fn quote(amount_a: u128, reserve_a: u128, reserve_b: u128) -> u128{
+		amount_a * reserve_b / reserve_a
+}
+	// }
 }
--- pallets/omni-swap/src/tests.rs
+++ pallets/omni-swap/src/tests.rs
@@ -1,96 +1,121 @@
 use crate::{mock::*, Error};
 use frame_support::{assert_noop, assert_ok};
 
-#[test]
-fn it_works_for_create_claim() {
-    new_test_ext().execute_with(|| {
-        let claim = vec![1, 2, 3, 4, 5, 6, 7, 8];
-        // Dispatch a signed extrinsic.
-        assert_ok!(PoeModule::create_claim(Origin::signed(1), claim.clone()));
-        // Read pallet storage and assert an expected result.
-        assert!(PoeModule::proofs(claim.clone()).is_some());
-    });
-}
+// #[test]
+// fn it_works_for_default_value() {
+// 	new_test_ext().execute_with(|| {
+// 		// Dispatch a signed extrinsic.
+// 		assert_ok!(TemplateModule::add_liquidity(RuntimeOrigin::signed(1), 42));
+// 		// Read pallet storage and assert an expected result.
+// 		// assert_eq!(TemplateModule::something(), Some(42));
+// 	});
+// }
 
-#[test]
-fn it_fails_for_create_claim_with_claim_length_error() {
-    new_test_ext().execute_with(|| {
-        let claim = vec![1, 2, 3];
-        // Dispatch a signed extrinsic.
-        assert_noop!(PoeModule::create_claim(Origin::signed(1), claim), Error::<Test>::ClaimLengthError);
-    });
-}
+// #[test]
+// fn correct_error_for_none_value() {
+// 	new_test_ext().execute_with(|| {
+// 		// Ensure the expected error is thrown when no value is present.
+// 		assert_noop!(
+// 			TemplateModule::cause_error(RuntimeOrigin::signed(1)),
+// 			Error::<Test>::NoneValue
+// 		);
+// 	});
+// }
 
-#[test]
-fn it_fails_for_create_claim_with_claim_alreay_exist() {
-    new_test_ext().execute_with(|| {
-        let claim = vec![1, 2, 3, 4, 5, 6, 7, 8];
-        // Dispatch a signed extrinsic.
-        assert_ok!(PoeModule::create_claim(Origin::signed(1), claim.clone()));
-        assert_noop!(PoeModule::create_claim(Origin::signed(1), claim), Error::<Test>::ClaimAlreadyExist);
-    });
-}
 
-#[test]
-fn it_fails_for_revoke_claim_with_claim_not_exist() {
-    new_test_ext().execute_with(|| {
-        let claim = vec![1, 2, 3, 4, 5, 6, 7, 8];
-        assert_noop!(PoeModule::revoke_claim(Origin::signed(1), claim), Error::<Test>::ClaimNotExist);
-    });
-}
+// use crate::{mock::*, Error};
+// use frame_support::{assert_noop, assert_ok};
 
-#[test]
-fn it_fails_for_revoke_claim_with_not_owner() {
-    new_test_ext().execute_with(|| {
-        let claim = vec![1, 2, 3, 4, 5, 6, 7, 8];
-        // Dispatch a signed extrinsic.
-        assert_ok!(PoeModule::create_claim(Origin::signed(1), claim.clone()));
-        assert_noop!(PoeModule::revoke_claim(Origin::signed(2), claim), Error::<Test>::OnlyOwnerCanRevoke);
-    });
-}
+// #[test]
+// fn it_works_for_create_claim() {
+//     new_test_ext().execute_with(|| {
+//         let claim = vec![1, 2, 3, 4, 5, 6, 7, 8];
+//         // Dispatch a signed extrinsic.
+//         assert_ok!(PoeModule::create_claim(Origin::signed(1), claim.clone()));
+//         // Read pallet storage and assert an expected result.
+//         assert!(PoeModule::proofs(claim.clone()).is_some());
+//     });
+// }
 
-#[test]
-fn it_works_for_revoke_claim() {
-    new_test_ext().execute_with(|| {
-        let claim = vec![1, 2, 3, 4, 5, 6, 7, 8];
-        // Dispatch a signed extrinsic.
-        assert_ok!(PoeModule::create_claim(Origin::signed(1), claim.clone()));
-        assert_ok!(PoeModule::revoke_claim(Origin::signed(1), claim));
-    });
-}
+// #[test]
+// fn it_fails_for_create_claim_with_claim_length_error() {
+//     new_test_ext().execute_with(|| {
+//         let claim = vec![1, 2, 3];
+//         // Dispatch a signed extrinsic.
+//         assert_noop!(PoeModule::create_claim(Origin::signed(1), claim), Error::<Test>::ClaimLengthError);
+//     });
+// }
 
-#[test]
-fn it_fails_for_transfer_claim_with_transfer_to_self() {
-    new_test_ext().execute_with(|| {
-        let claim = vec![1, 2, 3, 4, 5, 6, 7, 8];
-        assert_noop!(PoeModule::transfer_claim(Origin::signed(1), 1, claim), Error::<Test>::NotAbleToTransferToSelf);
-    });
-}
+// #[test]
+// fn it_fails_for_create_claim_with_claim_alreay_exist() {
+//     new_test_ext().execute_with(|| {
+//         let claim = vec![1, 2, 3, 4, 5, 6, 7, 8];
+//         // Dispatch a signed extrinsic.
+//         assert_ok!(PoeModule::create_claim(Origin::signed(1), claim.clone()));
+//         assert_noop!(PoeModule::create_claim(Origin::signed(1), claim), Error::<Test>::ClaimAlreadyExist);
+//     });
+// }
 
-#[test]
-fn it_fails_for_transfer_claim_with_claim_not_exist() {
-    new_test_ext().execute_with(|| {
-        let claim = vec![1, 2, 3, 4, 5, 6, 7, 8];
-        assert_noop!(PoeModule::transfer_claim(Origin::signed(1), 2, claim), Error::<Test>::ClaimNotExist);
-    });
-}
+// #[test]
+// fn it_fails_for_revoke_claim_with_claim_not_exist() {
+//     new_test_ext().execute_with(|| {
+//         let claim = vec![1, 2, 3, 4, 5, 6, 7, 8];
+//         assert_noop!(PoeModule::revoke_claim(Origin::signed(1), claim), Error::<Test>::ClaimNotExist);
+//     });
+// }
 
-#[test]
-fn it_fails_for_transfer_claim_with_not_owner() {
-    new_test_ext().execute_with(|| {
-        let claim = vec![1, 2, 3, 4, 5, 6, 7, 8];
-        // Dispatch a signed extrinsic.
-        assert_ok!(PoeModule::create_claim(Origin::signed(1), claim.clone()));
-        assert_noop!(PoeModule::transfer_claim(Origin::signed(2), 1, claim), Error::<Test>::OnlyOwnerCanTransfer);
-    });
-}
+// #[test]
+// fn it_fails_for_revoke_claim_with_not_owner() {
+//     new_test_ext().execute_with(|| {
+//         let claim = vec![1, 2, 3, 4, 5, 6, 7, 8];
+//         // Dispatch a signed extrinsic.
+//         assert_ok!(PoeModule::create_claim(Origin::signed(1), claim.clone()));
+//         assert_noop!(PoeModule::revoke_claim(Origin::signed(2), claim), Error::<Test>::OnlyOwnerCanRevoke);
+//     });
+// }
 
-#[test]
-fn it_works_for_transfer_claim() {
-    new_test_ext().execute_with(|| {
-        let claim = vec![1, 2, 3, 4, 5, 6, 7, 8];
-        // Dispatch a signed extrinsic.
-        assert_ok!(PoeModule::create_claim(Origin::signed(1), claim.clone()));
-        assert_ok!(PoeModule::transfer_claim(Origin::signed(1), 2, claim));
-    });
-}
+// #[test]
+// fn it_works_for_revoke_claim() {
+//     new_test_ext().execute_with(|| {
+//         let claim = vec![1, 2, 3, 4, 5, 6, 7, 8];
+//         // Dispatch a signed extrinsic.
+//         assert_ok!(PoeModule::create_claim(Origin::signed(1), claim.clone()));
+//         assert_ok!(PoeModule::revoke_claim(Origin::signed(1), claim));
+//     });
+// }
+
+// #[test]
+// fn it_fails_for_transfer_claim_with_transfer_to_self() {
+//     new_test_ext().execute_with(|| {
+//         let claim = vec![1, 2, 3, 4, 5, 6, 7, 8];
+//         assert_noop!(PoeModule::transfer_claim(Origin::signed(1), 1, claim), Error::<Test>::NotAbleToTransferToSelf);
+//     });
+// }
+
+// #[test]
+// fn it_fails_for_transfer_claim_with_claim_not_exist() {
+//     new_test_ext().execute_with(|| {
+//         let claim = vec![1, 2, 3, 4, 5, 6, 7, 8];
+//         assert_noop!(PoeModule::transfer_claim(Origin::signed(1), 2, claim), Error::<Test>::ClaimNotExist);
+//     });
+// }
+
+// #[test]
+// fn it_fails_for_transfer_claim_with_not_owner() {
+//     new_test_ext().execute_with(|| {
+//         let claim = vec![1, 2, 3, 4, 5, 6, 7, 8];
+//         // Dispatch a signed extrinsic.
+//         assert_ok!(PoeModule::create_claim(Origin::signed(1), claim.clone()));
+//         assert_noop!(PoeModule::transfer_claim(Origin::signed(2), 1, claim), Error::<Test>::OnlyOwnerCanTransfer);
+//     });
+// }
+
+// #[test]
+// fn it_works_for_transfer_claim() {
+//     new_test_ext().execute_with(|| {
+//         let claim = vec![1, 2, 3, 4, 5, 6, 7, 8];
+//         // Dispatch a signed extrinsic.
+//         assert_ok!(PoeModule::create_claim(Origin::signed(1), claim.clone()));
+//         assert_ok!(PoeModule::transfer_claim(Origin::signed(1), 2, claim));
+//     });
+// }
